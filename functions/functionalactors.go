280c9e210f59e44fd8df70752e52feca73082ccb
Seth Etter <sethetter@gmail.com>
2018-08-21 21:31:15 -0500

    Mux iteration 2, sharing memory by communicating

--- functions/functionalactors.go
+++ functions/functionalactors.go
@@ -1,35 +1,42 @@
 package main
 
 import (
+	"io"
 	"net"
-	"sync"
 )
 
 type Mux struct {
-	mu    sync.Mutex
-	conns map[net.Addr]net.Conn
+	add     chan net.Conn
+	remove  chan net.Addr
+	sendMsg chan string
 }
 
 func (m *Mux) Add(conn net.Conn) {
-	m.mu.Lock()
-	defer m.mu.Unlock()
-	m.conns[conn.RemoteAddr()] = conn
+	m.add <- conn
 }
 
 func (m *Mux) Remove(addr net.Addr) {
-	m.mu.Lock()
-	defer m.mu.Unlock()
-	delete(m.conns, addr)
+	m.remove <- addr
 }
 
 func (m *Mux) SendMsg(msg string) error {
-	m.mu.Lock()
-	defer m.mu.Unlock()
-	for _, conn := range m.conns {
-		err := io.WriteString(conn, msg)
-		if err != nil {
-			return err
+	m.sendMsg <- msg
+	return nil
+}
+
+func (m *Mux) loop() {
+	conns := make(map[net.Addr]net.Conn)
+	for {
+		select {
+		case conn := <-m.add:
+			conns[conn.RemoteAddr()] = conn
+		case addr := <-m.remove:
+			delete(conns, addr)
+		case msg := <-m.sendMsg:
+			for _, conn := range conns {
+				// TODO: err handling?!
+				io.WriteString(conn, msg)
+			}
 		}
 	}
-	return nil
 }
